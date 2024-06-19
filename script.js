c0eac9524a62f43ee22650d637591d8c39c7acf4
mooooddoooduudgugds <bhavnaarr78@gmail.com>
2024-06-19 11:31:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const title = document.getElementById("title");
+const desc = document.getElementById("desc")
+const sname = document.getElementById("name")
+
+
+
+title.innerText = "Not Inserting all the cringy names but yes Chahat<3";
+
+desc.innerText = "These past few months have been the best months of my life excluding the ghosting period and maybe you are a reason. I hope I get to wish you on your coming birthdays as well and yes idk how to write a speech soo that's it ig and once again a very happy happy birthday to you.";
+
+
